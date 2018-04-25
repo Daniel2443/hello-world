6a665e79d99bcf79315885639e8a575cb02e7e7e
luisk <luisk@192.168.1.6>
2018-04-25 14:46:48 -0600

    modificacion de algunos metodos y se añadio el metodo para eliminar

--- src/Funciones.java
+++ src/Funciones.java
@@ -10,18 +10,19 @@ import java.util.ArrayList;
  */
 public class Funciones {
 
-	public void mezclar(BinaryTree a, BinaryTree b) {
+	public BinaryTree mezclar(BinaryTree a, BinaryTree b) {
 		ArrayList<Integer> x = a.inOrder2();
 		ArrayList<Integer> y = b.inOrder2();
 
 		BinaryTree c = new BinaryTree();
 		for (int i = 0; i < x.size(); i++) {
-			c.add(x.get(i));
+			c.insert(x.get(i));
 
 		}
 		for (int i = 0; i < y.size(); i++) {
-			c.add(y.get(i));
+			c.insert(y.get(i));
 		}
 		c.inOrder();
+		return c;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -18,22 +18,48 @@ public class Main {
 		Funciones fn = new Funciones();
 		
 		
-		bt.add(5);
-		bt.add(2);
-		bt.add(7);
-		bt.add(9);
-		bt.add(3);
-		bt.add(1);
-		bt.diffAux();
+		bt.insert(5);
+		bt.insert(2);
+		bt.insert(7);
+		bt.insert(9);
+		bt.insert(3);
+		bt.insert(1);
+		bt.inOrder();
+		bt.postOrder();
+		bt.preOrder();
+		System.out.println();
+		bt.diff();
 		BinaryTree vb = new BinaryTree();
-		vb.add(4);
-		vb.add(7);
-		vb.add(3);
-		vb.add(8);
-		vb.add(10);
-		vb.add(5);
-		vb.add(6);
-		fn.mezclar(bt, vb);
+		System.out.println();
+		vb.insert(4);
+		vb.insert(7);
+		vb.insert(3);
+		vb.insert(8);
+		vb.insert(10);
+		vb.insert(5);
+		vb.insert(6);
+		vb.inOrder();
+		vb.postOrder();
+		vb.preOrder();
+		System.out.println();
+		BinaryTree c = fn.mezclar(bt, vb);
+		System.out.println();
+		c.inOrder();
+		c.postOrder();
+		c.preOrder();
+		System.out.println();
+		c.diff();
+		System.out.println();
+		c.eliminar(5);
+		System.out.println("dice que se elimino el 6 pero es porque copia ese valor en el nodo que contenia \n"
+				+ "el 5 y luego elimina el nodo hoja con el valor 6");
+		c.inOrder();
+		System.out.println();
+		c.eliminar(1);
+		c.eliminar(8);
+		c.inOrder();
+		System.out.println();
+		c.eliminar(5);
 		
 		
 		
